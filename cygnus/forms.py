3f4b30a42198e5bb9bed13f32887675cd9c0edd6
Franklin <franklinbello99@gmail.com>
2019-12-11 02:45:06 -0500

    Included validation methods to prevent creating Users with already existing information. Created the NewCourseForm to add a form

--- cygnus/forms.py
+++ cygnus/forms.py
@@ -1,15 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, DateField, SelectMultipleField
-from wtforms.validators import DataRequired, InputRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, InputRequired, Length, Email, EqualTo, ValidationError
+from cygnus.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField  ('Username', validators=[DataRequired(), Length(min=3, max=20)])
     email    = StringField  ('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
     confirm  = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    # date = DateField('Date')
-    # account = SelectField('Account', validators=[DataRequired()], coerce='Unicode', choices=[('1', 'Student'), ('2', 'Tutor')])
     submit   = SubmitField  ('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already registered.')
 
 class LoginForm(FlaskForm):
     email    = StringField  ('Email', validators=[DataRequired(), Email()])
@@ -25,23 +34,9 @@ class SearchForm(FlaskForm):
     search  = StringField('SearchBar')
     submit  = SubmitField('SearchBtn')
 
+class NewCourseForm(FlaskForm):
+    name = StringField('Course Name')
+    description = StringField('Course Description')
+
 class UserProfileForm(FlaskForm):
-    # name = StringField('Name')
-    # last_name = StringField('Last Name')
-    # bio = TextAreaField('Bio')
-    # school = StringField('School')
-    # campus = StringField('Campus')
-    # location = StringField('Location')
-    # major = StringField('Major')
-    # dob = DateField('Date of Birth')
-    # year_graduation = DateField('Graduation Year')
-    # course_list = SelectMultipleField()
-    # image
     pass
-
-
-# Add recaptcha field to avoid bots
-# If you are a student then submit
-#   Unofficial transcripts to verify and one letter of recommendation
-# Else
-#   Only unofficial transcripts
