46c6e53cd7361385434dc322661998264119ca09
Franklin <franklinbello99@gmail.com>
2019-12-11 02:42:10 -0500

    Renamed the attributes of the User and Post class. Created the Course model

--- cygnus/models.py
+++ cygnus/models.py
@@ -3,19 +3,25 @@ from cygnus import db
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f"User('{self.username}', '{self.email}')"
 
 class Post(db.Model):
     post_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(64))
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    title = db.Column(db.String(64), nullable=False)
+    body = db.Column(db.String(140), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return f"Post('{self.title}', '{self.body}', '{self.date_created}')"
+
+class Course(db.Model):
+    course_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(140), nullable=False)
