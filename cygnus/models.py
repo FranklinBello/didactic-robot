10f1818c1b98cdbed485a0a03ffcaf00137cf15e
Franklin <franklinbello99@gmail.com>
2019-12-11 03:53:50 -0500

    Changed views so it authenticates users. Imported the packages in __init__. Linked User with the Login_Manager

--- cygnus/models.py
+++ cygnus/models.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from cygnus import db
+from cygnus import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
@@ -12,16 +17,16 @@ class User(db.Model):
         return f"User('{self.username}', '{self.email}')"
 
 class Post(db.Model):
-    post_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), nullable=False)
     body = db.Column(db.String(140), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.body}', '{self.date_created}')"
 
 class Course(db.Model):
-    course_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     description = db.Column(db.String(140), nullable=False)
--- cygnus/views.py
+++ cygnus/views.py
@@ -1,6 +1,10 @@
-from flask import render_template, redirect, url_for
-from cygnus import app
+from flask import render_template, request, redirect, url_for, flash
+
+from cygnus import app, db, bcrypt
 from cygnus.forms import RegistrationForm, LoginForm
+from cygnus.models import User
+
+from flask_login import login_user
 
 @app.route('/')
 @app.route('/home')
@@ -11,14 +15,26 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@cygnus.com' and form.password.data == 'password':
+        user = User.query.filter_by(email=form.email.data,).first()
+        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password', 'danger')
+    flash(f'{form.errors}', 'danger')
     return render_template('public/login.html', header='Login', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        password_hash = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, 
+                    email=form.email.data, 
+                    password_hash=password_hash)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account was created', 'success')
         return redirect(url_for('login'))
     return render_template('public/register.html', title='Register', form=form)
 
@@ -34,6 +50,6 @@ def Profile():
 def about():
     return render_template('public/about.html')
 
-@app.route('/404')
-def Error404():
+@app.errorhandler(404)
+def page_not_found(error):
     return render_template('public/404.html', title='Not Found')
