6eeb8ae6d1bda017bf5630a77fe2afc027b132be
Franklin <franklinbello99@gmail.com>
2019-12-12 04:29:46 -0500

    Changed user model, added first name, last name, image file, ge file, city, state, zip code, and bio fields

--- cygnus/models.py
+++ cygnus/models.py
@@ -6,12 +6,27 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+studying = db.Table('studying',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), nullable=False),
+    db.PrimaryKeyConstraint('user_id', 'course_id'))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    image_file = db.Column(db.String(16), default='default.jpg')
+    city = db.Column(db.String(20))
+    state = db.Column(db.String(20))
+    zip_code = db.Column(db.String(10))
+    bio = db.Column(db.String(140))
+
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    courses = db.relationship('Course', secondary=studying, backref='student')
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
@@ -21,7 +36,8 @@ class Post(db.Model):
     title = db.Column(db.String(64), nullable=False)
     body = db.Column(db.String(140), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.body}', '{self.date_created}')"
@@ -30,3 +46,7 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     description = db.Column(db.String(140), nullable=False)
+
+    def __repr__(self):
+        return f"Course('{self.name}', '{self.description}')"
+
